36e4fae6b44d16635b0f80eccc2007b42f7cd479
Ami Bhatt <ami@milyoni.com>
2013-06-27 16:54:24 -0700

    test defining connection in migration

--- db/migrate/20130626005025_create_batch_drivers.rb
+++ db/migrate/20130626005025_create_batch_drivers.rb
@@ -1,8 +1,14 @@
 class CreateBatchDrivers < ActiveRecord::Migration
+  def connection
+    @connection ||= ActiveRecord::Base.establish_connection("#{Rails.env}").connection
+  end
+
   def change
     create_table :batch_drivers do |t|
       t.string :job
       t.string :key
     end
+
+    @connection = ActiveRecord::Base.establish_connection("#{Rails.env}").connection
   end
 end
