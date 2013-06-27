aaecb116fb06ce88c00684220b8e6973c8853137
Ami Bhatt <ami@milyoni.com>
2013-06-27 16:07:08 -0700

    test env vars

--- app/models/sumuru_call.rb
+++ app/models/sumuru_call.rb
@@ -1,4 +1,10 @@
 class SumuruCall < ActiveRecord::Base
   self.abstract_class = true
-  ActiveRecord::Base.establish_connection 'sumuru_db'
+  ActiveRecord::Base.establish_connection(
+    :database => "#{ENV['DUB']}",
+    :username => "#{ENV['DUB_UN']}",
+    :password => "#{ENV['DUB_PWD']}",
+    :host => "#{ENV['DUB_HOST']}",
+    :port => "#{ENV['DUB_PORT']}",
+  )
 end
