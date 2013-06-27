bcc8546ed0d8e41010e26890a5c19cb575145185
Ami Bhatt <ami@milyoni.com>
2013-06-27 16:47:33 -0700

    stupid migrations

--- db/migrate/20130627221411_add_column_facebook_id_to_user_fact.rb
+++ db/migrate/20130627221411_add_column_facebook_id_to_user_fact.rb
@@ -1,6 +1,5 @@
 class AddColumnFacebookIdToUserFact < ActiveRecord::Migration
   def change
     add_column :user_facts, :facebook_user_id, :string
-    add_column :user_facts, :user_id, :integer
   end
 end
