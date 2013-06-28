c3f20b4b523a3ccdd8f209b4dc952b4f59304888
Ami Bhatt <ami@milyoni.com>
2013-06-28 12:12:15 -0700

    turn off sprockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Userfacts::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.initialize_on_precompile = false
+
   config.assets.compile = false
 
 
