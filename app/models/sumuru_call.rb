ec70f9af2b90f7d23e7cd966d0c75e9ff4d69e0d
Ami Bhatt <ami@milyoni.com>
2013-06-28 10:29:06 -0700

    fix adapter error on deploy

--- app/models/sumuru_call.rb
+++ app/models/sumuru_call.rb
@@ -1,13 +1,13 @@
 class SumuruCall < ActiveRecord::Base
   self.abstract_class = true
-  #ActiveRecord::Base.establish_connection(
-  #  :adapter => 'postgresql',
-  #  :database => "#{ENV['DUB']}",
-  #  :username => "#{ENV['DUB_UN']}",
-  #  :password => "#{ENV['DUB_PWD']}",
-  #  :host => "#{ENV['DUB_HOST']}",
-  #  :port => "#{ENV['DUB_PORT']}",
-  #)
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :database => "#{ENV['DUB']}",
+    :username => "#{ENV['DUB_UN']}",
+    :password => "#{ENV['DUB_PWD']}",
+    :host => "#{ENV['DUB_HOST']}",
+    :port => "#{ENV['DUB_PORT']}",
+  )
 
-  establish_connection 'sumuru'
+  #establish_connection 'sumuru'
 end
