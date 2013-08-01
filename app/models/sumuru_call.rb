9ba5c8085020bd966220a2e372e8b2b99f56b6fd
Ami Bhatt <ami@milyoni.com>
2013-08-01 11:00:48 -0700

    revert

--- app/models/sumuru_call.rb
+++ app/models/sumuru_call.rb
@@ -1,12 +1,23 @@
 class SumuruCall < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection(
-    ENV['USER_SOURCE_DATA'] ||
+  if ENV['DUB']
+    establish_connection(
+      :adapter => 'postgresql',
+      :database => "#{ENV['DUB']}",
+      :username => "#{ENV['DUB_UN']}",
+      :password => "#{ENV['DUB_PWD']}",
+      :host => "#{ENV['DUB_HOST']}",
+      :port => "#{ENV['DUB_PORT']}")
+  else
+    establish_connection(
+      ENV['USER_SOURCE_DATA'] ||
       :adapter => "postgresql",
-    :database => "d14m3t0ljark4i",
-    :username => "ud9lu32qsk4bht",
-    :password => "pb0gh4fovd2vb0febgruu3tg3rv",
-    :host => "ec2-54-214-28-104.us-west-2.compute.amazonaws.com",
-    :port => "5472"
-  )
+      :database => "d14m3t0ljark4i",
+      :username => "ud9lu32qsk4bht",
+      :password => "pb0gh4fovd2vb0febgruu3tg3rv",
+      :host => "ec2-54-214-28-104.us-west-2.compute.amazonaws.com",
+      :port => "5472"
+    )
+  end
 end
+
