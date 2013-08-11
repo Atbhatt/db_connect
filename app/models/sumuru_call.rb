3131e4323a85d6c7f45083110437b5be3b39dbeb
Ami Bhatt <atbhatt@msn.com>
2013-08-10 18:24:52 -0700

    first commit

--- app/models/sumuru_call.rb
+++ app/models/sumuru_call.rb
@@ -9,15 +9,7 @@ class SumuruCall < ActiveRecord::Base
       :host => "#{ENV['DUB_HOST']}",
       :port => "#{ENV['DUB_PORT']}")
   else
-    establish_connection(
-      ENV['USER_SOURCE_DATA'] ||
-      :adapter => "postgresql",
-      :database => "d14m3t0ljark4i",
-      :username => "ud9lu32qsk4bht",
-      :password => "pb0gh4fovd2vb0febgruu3tg3rv",
-      :host => "ec2-54-214-28-104.us-west-2.compute.amazonaws.com",
-      :port => "5472"
-    )
+    puts "error connecting"
   end
 end
 
